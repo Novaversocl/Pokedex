e1b3c34e836e0cb301409990dfd6919da5713d02
Juan luis Estrada <95386670+Novaversocl@users.noreply.github.com>
2024-06-11 17:16:25 -0400

    listado pokemon

--- src/helpers/fetchPokemon.js
+++ src/helpers/fetchPokemon.js
@@ -1,22 +1,24 @@
 import axios from 'axios';
 import { URL_BASE_POKEMON } from '@/constants/urlConstants';
 
-export const getPokemonPerPage = async (limit = 20, offset = 0) => {
+export const getPokemonPerPage = async (start = 251, end = 385) => {
     try {
+        const limit = end - start;
+        const offset = start;
+
         const { data } = await axios.get(URL_BASE_POKEMON, {
             params: {
                 limit,
                 offset,
             },
         });
-        
+
         const pokemonDetailsPromises = data.results.map(async (pokemon) => {
             const details = await axios.get(pokemon.url);
             return {
                 name: pokemon.name,
                 image: details.data.sprites.other.dream_world.front_default,
-// imagen en hd svg
-
+                // imagen en hd svg
             };
         });
 
